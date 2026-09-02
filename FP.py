1fc30529483cde6008c83902f94cc15e90577a9a
Greg Rossi <gregoriorossi14@gmail.com>
2026-09-02 12:44:13 -0300

    First guessing game in FP.py

---
+++ FP.py
@@ -0,0 +1,28 @@
+import random
+
+print("Joguin")
+print("Adivinhe o número que estou penando entre 1 e 100")
+print("Você tem 7 tentativas")
+
+numero_secreto = random.randint(1,100)
+contador = 7
+acertou = False
+
+while contador > 0:
+    print("Você ainda tem", contador, "tentativas")
+    tentativa = int(input("digite seu palpite: "))
+    contador -=1
+
+    if tentativa == numero_secreto:
+        print("Parabéns, ta certo")
+        acertou = True
+        break
+    elif tentativa < numero_secreto:
+        print("o número secreto é MAIOR que seu palpite")
+    else:
+        print("o número secreto é MENOR que seu palpite")
+
+if not acertou:
+    print("Errrooou, o número certo é:", numero_secreto)
+else:
+    print ("Parabéns, ta certo")
